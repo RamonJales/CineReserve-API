584e714c0590e73e0a9b59a1214adafe8c254779
RamonJales <ramonjales123@gmail.com>
2026-03-22 22:40:53 -0300

    chore: ruff application

--- core/settings.py
+++ core/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
 from environs import Env
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
